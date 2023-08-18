75bd80295610a6d69ceff6cb24b0c6694887b563
Lyubo <lubodimoff@gmail.com>
2023-08-18 17:55:58 +0300

    update URL login/sign up microservice

--- src/main/java/webapplication/gatewayapi/GatewayApiApplication.java
+++ src/main/java/webapplication/gatewayapi/GatewayApiApplication.java
@@ -19,9 +19,7 @@ public class GatewayApiApplication {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("usersRegistration", r -> r.path("/usersRegistration/**")
-                        .uri("http://usersRegistration:8081")) // Replace with your microservice URL
-                .route("userAccount", r -> r.path("/userAccount/**")
-                        .uri("http://userAccount:8082")) // Replace with your microservice URL
+                        .uri("http://localhost:5001")) // Replace with your microservice URL
                 .build();
     }
 
