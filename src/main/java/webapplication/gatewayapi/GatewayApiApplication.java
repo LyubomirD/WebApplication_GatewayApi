8e918d8d97de9a9f5478cc35e38206f042c9a511
Lyubo <lubodimoff@gmail.com>
2023-08-19 18:38:14 +0300

    working with Postman, CORS error with frontend

--- src/main/java/webapplication/gatewayapi/GatewayApiApplication.java
+++ src/main/java/webapplication/gatewayapi/GatewayApiApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:63343")
 public class GatewayApiApplication {
 
     public static void main(String[] args) {
